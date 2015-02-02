227dcf5280552ad7fa1d1633623fd1a751b4f6d8
clampz <david@weinman.com>
2015-02-02 00:32:40 -0800

    updated blue team submitter, now uses the ids part of the DSL and creates necassary directories for challenges

--- blue_team_submitter/SubmissionDaemon.cs
+++ blue_team_submitter/SubmissionDaemon.cs
@@ -9,12 +9,9 @@
 
 using System;
 using System.IO;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Collections;
 using System.Diagnostics;
 using System.ServiceProcess;
-using System.Windows.Forms;
 using System.Net.Sockets;
 
 namespace SubmissionDaemon
@@ -25,32 +22,23 @@ namespace SubmissionDaemon
         const Int32 port = 7890;
         const String scoringServer = "10.0.27.128";
 
-        // maximum submission size in bytes (10K).
-        const int MAXSUBMISSIONLENGTH = 10000;
+        // maximum submission size in bytes.
+        const int MAXSUBMISSIONLENGTH = 3000;
 
-        // challenge submission filename constants
-        const String chall1_filename = "ftp_server.txt";
-        const String chall2_filename = "forgot_my_pass.txt";
-        const String chall3_filename = "compress_stan.txt";
-        const String chall4_filename = "stan.txt";
-        const String chall5_filename = "pro_tools.txt";
-        const String chall6_filename = "plugin.txt";
-        const String chall1_foldername = "ftp_service\\";
-        const String chall2_foldername = "forgot_my_pass\\";
-        const String chall3_foldername = "compress_stan\\";
-        const String chall4_foldername = "stan\\";
-        const String chall5_foldername = "pro_tools\\";
-        const String chall6_foldername = "plugin\\";
+        // number of challenges
+        const int num_challs = 6;
 
+        // challenge names
+        string[] chall_names = new string[num_challs];
+
+        // challenge file name to id relations
+        static Hashtable chall_ids = new Hashtable();
+
+        // array of file watcher objects
+        FileSystemWatcher[] filewatchers = new FileSystemWatcher[num_challs];
+
+        //const String submissions_path = "C:\\Users\\blue_team\\Documents\\submissions\\";
         const String submissions_path = "C:\\Users\\Administrator\\Documents\\submissions\\";
-
-        // challenge file watcher objects
-        FileSystemWatcher chall1_filewatcher;
-        FileSystemWatcher chall2_filewatcher;
-        FileSystemWatcher chall3_filewatcher;
-        FileSystemWatcher chall4_filewatcher;
-        FileSystemWatcher chall5_filewatcher;
-        FileSystemWatcher chall6_filewatcher;
 
         // submission daemon constructor
         public SubmissionDaemon()
@@ -80,7 +68,7 @@ namespace SubmissionDaemon
         {
             String currentContent;
             // log the change
-            LogEvent(String.Format("File {0} was: {1}", evntargs.FullPath, evntargs.ChangeType.ToString()));
+            LogEvent(String.Format("File with name {0} was: {1}", evntargs.Name, evntargs.ChangeType.ToString()));
             // then try to read in the file data and send to submission listener
             try
             {
@@ -106,7 +94,7 @@ namespace SubmissionDaemon
                 // initialize the tcp client to send the submission to
                 TcpClient client = new TcpClient(scoringServer, port);
                 // make a byte array our of the submission
-                Byte[] submission = System.Text.Encoding.ASCII.GetBytes(currentContent);
+                Byte[] submission = System.Text.Encoding.ASCII.GetBytes(String.Format("CHALLENGE_SUBMIT: blue{0}\nBEGIN\n{1}\nEND", chall_ids[evntargs.Name].ToString(), currentContent));
                 // prepare client stream to send submission to 
                 NetworkStream stream = client.GetStream();
                 // Log submission event
@@ -145,46 +133,46 @@ namespace SubmissionDaemon
         protected override void OnStart(string[] args)
         {
             base.OnStart(args);
-            // initialize file watcher objects
-            chall1_filewatcher = new FileSystemWatcher(submissions_path + chall1_foldername, chall1_filename);
-            chall2_filewatcher = new FileSystemWatcher(submissions_path + chall2_foldername, chall2_filename);
-            chall3_filewatcher = new FileSystemWatcher(submissions_path + chall3_foldername, chall3_filename);
-            chall4_filewatcher = new FileSystemWatcher(submissions_path + chall4_foldername, chall4_filename);
-            chall5_filewatcher = new FileSystemWatcher(submissions_path + chall5_foldername, chall5_filename);
-            chall6_filewatcher = new FileSystemWatcher(submissions_path + chall6_foldername, chall6_filename);
-            chall1_filewatcher.EnableRaisingEvents = true;
-            chall2_filewatcher.EnableRaisingEvents = true;
-            chall3_filewatcher.EnableRaisingEvents = true;
-            chall4_filewatcher.EnableRaisingEvents = true;
-            chall5_filewatcher.EnableRaisingEvents = true;
-            chall6_filewatcher.EnableRaisingEvents = true;
+            int i;
 
-            // set notification events
-            chall1_filewatcher.NotifyFilter = NotifyFilters.FileName;
-            chall2_filewatcher.NotifyFilter = NotifyFilters.FileName;
-            chall3_filewatcher.NotifyFilter = NotifyFilters.FileName;
-            chall4_filewatcher.NotifyFilter = NotifyFilters.FileName;
-            chall5_filewatcher.NotifyFilter = NotifyFilters.FileName;
-            chall6_filewatcher.NotifyFilter = NotifyFilters.FileName;
+            // make challenge names
+            chall_names[0] = "ftp_server";
+            chall_names[1] = "stan";
+            chall_names[2] = "plugin";
+            chall_names[3] = "forgot_my_pass";
+            chall_names[4] = "compress_stan";
+            chall_names[5] = "pro_tools";
 
-            // register event handler that
-            // gets called when the file is created
-            chall1_filewatcher.Created += new FileSystemEventHandler(OnCreate);
-            chall2_filewatcher.Created += new FileSystemEventHandler(OnCreate);
-            chall3_filewatcher.Created += new FileSystemEventHandler(OnCreate);
-            chall4_filewatcher.Created += new FileSystemEventHandler(OnCreate);
-            chall5_filewatcher.Created += new FileSystemEventHandler(OnCreate);
-            chall6_filewatcher.Created += new FileSystemEventHandler(OnCreate);
-
-            //  Register a handler that gets called if the  
-            //  challenge file watcher needs to report an error.
-            chall1_filewatcher.Error += new ErrorEventHandler(OnError);
-            chall2_filewatcher.Error += new ErrorEventHandler(OnError);
-            chall3_filewatcher.Error += new ErrorEventHandler(OnError);
-            chall4_filewatcher.Error += new ErrorEventHandler(OnError);
-            chall5_filewatcher.Error += new ErrorEventHandler(OnError);
-            chall6_filewatcher.Error += new ErrorEventHandler(OnError);
-
+            for (i = 0; i < num_challs; i++)
+            {
+                // try to make the challenge directory, exit if this fails (should never happen)
+                try {
+                    Directory.CreateDirectory(submissions_path + chall_names[i]);
+                } catch (Exception e) {
+                    LogEvent(String.Format("Submission Daemon is stopping because it couldn't create directory: {0}", e));
+                    this.Stop();
+                }
+                // make challenge file name relations id
+                chall_ids[chall_names[i] + ".txt"] = i + 1;
+                // initialize file watcher object
+                filewatchers[i] = new FileSystemWatcher(submissions_path + chall_names[i] + "\\", chall_names[i] + ".txt");
+                filewatchers[i].EnableRaisingEvents = true;
+                // set notification event
+                filewatchers[i].NotifyFilter = NotifyFilters.FileName;
+                // register event handler that
+                // gets called when the file is created
+                filewatchers[i].Created += new FileSystemEventHandler(OnCreate);
+                // register a handler that gets called if the
+                // challenge file watcher needs to report an error
+                filewatchers[i].Error += new ErrorEventHandler(OnError);
+            }
+            LogEvent("The FileSystemWatcher has been started on these files: \n"
+               submissions_path + chall_names[0] + "\\" + chall_names[0] + ".txt,\n"
+               submissions_path + chall_names[1] + "\\" + chall_names[1] + ".txt,\n"
+               submissions_path + chall_names[2] + "\\" + chall_names[2] + ".txt,\n"
+               submissions_path + chall_names[3] + "\\" + chall_names[3] + ".txt,\n"
+               submissions_path + chall_names[4] + "\\" + chall_names[4] + ".txt, and:\n"
+               submissions_path + chall_names[5] + "\\" + chall_names[5] + ".txt.");
         }
 
         protected override void OnStop()
@@ -196,14 +184,6 @@ namespace SubmissionDaemon
         public void Start()
         {
             OnStart(new string[0]);
-            LogEvent("The FileSystemWatcher has been started on these files: "
-    + submissions_path + chall1_foldername + chall1_filename + ",\n"
-    + submissions_path + chall2_foldername + chall2_filename + ",\n"
-    + submissions_path + chall3_foldername + chall3_filename + ",\n"
-    + submissions_path + chall4_foldername + chall4_filename + ",\n"
-    + submissions_path + chall5_foldername + chall5_filename + ", and:\n"
-    + submissions_path + chall6_foldername + chall6_filename + "."
-            );
         }
 
         public void Stop()
@@ -212,3 +192,4 @@ namespace SubmissionDaemon
         }
     }
 }
+
