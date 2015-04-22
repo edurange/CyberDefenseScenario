d4cc2417cc6f9a0ed20f2eb5879e46c9b467c858
clampz <david@weinman.com>
2015-04-21 20:37:33 -0700

    moved uptime_checker zip and updated links to chef scripts

---
+++ resources/chef/blueteam_default.rb
@@ -0,0 +1,34 @@
+#
+# Cookbook Name:: blueteam_default.rb
+# Recipe:: default
+#
+# setup the blueteam instance for the
+# cyber defense scenario
+
+# download the submission utility
+remote_file "SubmissionDaemon" do
+  source "https://github.com/edurange/CyberDefenseScenario/raw/master/blue_team_submitter/SubmissionDaemon.exe"
+  path "C:\\Users\\Administrator\\Documents\\SubmissionDaemon.exe"
+end
+
+# and its installer
+remote_file "submission_install_script" do
+  source "https://github.com/edurange/CyberDefenseScenario/raw/master/blue_team_submitter/submission_install.bat"
+  path "C:\\Users\\Administrator\\Documents\\submission_install.bat"
+end
+
+# plus an uninstaller - just in case
+remote_file "submission_uninstall_script" do
+  source "https://github.com/edurange/CyberDefenseScenario/raw/master/blue_team_submitter/submission_uninstall.bat"
+  path "C:\\Users\\Administrator\\Documents\\submission_uninstall.bat"
+end
+
+# also install start the utility since it runs in the background
+powershell_script "install_start_submission_sv" do
+  cwd "C:\\Users\\Administrator\\Documents"
+  code <<-EOH
+  Unblock-File SubmissionDaemon.exe
+  .\\submission_install.bat
+  sasv "SubmissionDaemon"
+  EOH
+end
